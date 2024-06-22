ee5904366f057d3789c67994e5ac0c6e0cae3ff4
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-22 15:37:49 +0530

    fetch car data from Rapid API

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import { CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars()
+  console.log(allCars);
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -16,6 +22,16 @@ export default function Home() {
             <CustomFilter />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            Cars Fetched
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
