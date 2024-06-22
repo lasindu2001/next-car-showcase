12e79e40a5b9b4a1187c6ba86d08ad7e40816529
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-23 01:03:27 +0530

    generate car images

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { CarProps } from "@/types";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import Image from "next/image";
 import { useState } from "react";
 import CustomButton from "./CustomButton";
@@ -30,7 +30,7 @@ const CarCard = ({ car }: CarCardProps) => {
         <span className='self-end text-[14px] leading-[17px] font-medium'>/day</span>
       </p>
       <div className='relative w-full h-40 my-3 object-contain'>
-        <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
+        <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
       </div>
       <div className='relative flex w-full mt-2'>
         <div className='flex group-hover:invisible w-full justify-between text-grey'>
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
     const headers: HeadersInit = {
         "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
@@ -5,10 +7,10 @@ export async function fetchCars() {
     };
 
     const response = await fetch(
-        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla`,
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera`,
         { headers: headers }
     );
-    
+
     const result = await response.json();
     return result;
 }
@@ -17,10 +19,25 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
     const mileageFactor = 0.1; // Additional rate per mile driven
     const ageFactor = 0.05; // Additional rate per year of vehicle age
-  
+
     const mileageRate = city_mpg * mileageFactor;
     const ageRate = (new Date().getFullYear() - year) * ageFactor;
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-  
+
     return rentalRatePerDay.toFixed(0);
-  };
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`;
+} 
